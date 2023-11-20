c5a0da00692051cea60f6face241c06f192f73fd
isadhwani <isadhwani16@gmail.com>
2023-11-20 18:24:30 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
+import session from "express-session";
+
+import cors from "cors";
+const app = express();
+// app.use(express.json);
+app.use(cors());
+CourseRoutes(app);
+ModuleRoutes(app);
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
+
---
+++ modules/routes.js
@@ -0,0 +1,64 @@
+import db from "../Database/index.js";
+
+function ModuleRoutes(app) {
+    app.get("/api/modules", (req, res) => {
+        const modules = db.modules;
+        res.json(modules);
+    });
+
+    app.get("/api/courses/:cid/modules", (req, res) => {
+        console.log("getting modules for course ", req.params.cid)
+        const { cid } = req.params;
+        const modules = db.modules
+            .filter((m) => m.course === cid);
+        res.send(modules);
+    });
+
+
+    app.get("/api/modules/:id", (req, res) => {
+        const { id } = req.params;
+        const module = db.modules.find((module) => module._id === id);
+        if (!module) {
+            res.status(404).send("Module not found");
+            return;
+        }
+        res.json(module);
+    });
+
+    app.delete("/api/modules/:mid", (req, res) => {
+        const { mid } = req.params;
+        db.modules = db.modules.filter((m) => m._id !== mid);
+        res.sendStatus(200);
+    });
+
+
+    app.post("/api/courses/:cid/modules", (req, res) => {
+        const { cid } = req.params;
+        console.log("adding module to course ", cid);
+        console.log("module is ", req.body);
+        const newModule = {
+            ...req.body,
+            course: cid,
+            _id: new Date().getTime().toString(),
+        };
+        db.modules.push(newModule);
+        res.send(newModule);
+    });
+
+
+    app.put("/api/modules/:mid", (req, res) => {
+        console.log("updating module ", req.params.mid + 
+            " with ", JSON.stringify(req.body));
+        const { mid } = req.params;
+        const moduleIndex = db.modules.findIndex(
+            (m) => m._id === mid);
+        db.modules[moduleIndex] = {
+            ...db.modules[moduleIndex],
+            ...req.body
+        };
+        res.sendStatus(204);
+    });
+
+}
+
+export default ModuleRoutes;
