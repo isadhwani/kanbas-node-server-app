046f99db419ef640524af83b735a58fd9e7fd410
isadhwani <isadhwani16@gmail.com>
2023-12-04 23:19:32 -0500

    reset some stuff

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(session(sessionOptions));
 
 
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;// || 'mongodb://127.0.0.1:27017/kanbas'
 mongoose.connect(CONNECTION_STRING);
 
 app.use(express.json());
