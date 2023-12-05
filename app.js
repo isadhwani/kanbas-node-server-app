6ba4c00295135b37b79dd2fef5c5f62e03683d42
isadhwani <isadhwani16@gmail.com>
2023-12-04 20:14:35 -0500

    finished half

--- app.js
+++ app.js
@@ -4,14 +4,36 @@ import express from "express";
 import CourseRoutes from "./modules/routes.js";
 import ModuleRoutes from "./courses/routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
 
 const app = express();
+app.use(
+ cors({
+   credentials: true,
+   origin: "http://localhost:3000",
+ })
+);
 
-app.use(cors());
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  app.use(
+    session(sessionOptions)
+  );
+  
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+
 app.use(express.json());
 Lab5(app);
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
+
 
 app.listen(process.env.PORT || 4000);
 
